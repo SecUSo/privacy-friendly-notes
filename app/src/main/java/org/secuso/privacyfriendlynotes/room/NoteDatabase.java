e86cc2a0da5419db041541bd3f91aef1f0b1e688
maxmitz <m-zimmermann1@gmx.net>
2021-12-03 09:34:10 +0100

    WIP(added migration implementation for testing purposes)

--- app/src/main/java/org/secuso/privacyfriendlynotes/room/NoteDatabase.java
+++ app/src/main/java/org/secuso/privacyfriendlynotes/room/NoteDatabase.java
@@ -3,6 +3,7 @@ package org.secuso.privacyfriendlynotes.room;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.room.AutoMigration;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
@@ -48,13 +49,33 @@ public abstract class NoteDatabase extends RoomDatabase {
         @Override
         public void migrate(SupportSQLiteDatabase database) {
             database.execSQL(
-                    "CREATE TABLE notifications_new (_noteId INTEGER NOT NULL,"
-                            + "time INTEGER,"
+                    "CREATE TABLE notifications_new (_noteId INTEGER NOT NULL DEFAULT 0,"
+                            + "time INTEGER NOT NULL DEFAULT 0,"
                             + "PRIMARY KEY(_noteId))");
-            database.execSQL("INSERT INTO notifications_new(_noteId, time)"
-                + "SELECT note, time FROM notifications");
+            database.execSQL("INSERT INTO notifications_new(_noteId, time) SELECT note, time FROM notifications");
             database.execSQL("DROP TABLE notifications");
             database.execSQL("ALTER TABLE notifications_new RENAME TO notifications");
+
+            database.execSQL(
+                    "CREATE TABLE notes_new (_id INTEGER NOT NULL DEFAULT 0,"
+                            + "in_trash INTEGER NOT NULL DEFAULT 0,"
+                            + "name TEXT NOT NULL DEFAULT 'TEXT',"
+                            + "type INTEGER NOT NULL DEFAULT 0,"
+                            + "category INTEGER NOT NULL DEFAULT 0,"
+                            + "content TEXT NOT NULL DEFAULT 'TEXT',"
+                            + "PRIMARY KEY(_id))");
+            database.execSQL("INSERT INTO notes_new(_id, in_trash,name,type,category,content) SELECT _id, in_trash,name,type,category,content FROM notes");
+            database.execSQL("DROP TABLE notes");
+            database.execSQL("ALTER TABLE notes_new RENAME TO notes");
+
+            database.execSQL(
+                    "CREATE TABLE categories_new (_id INTEGER NOT NULL DEFAULT 0,"
+                            + "name TEXT NOT NULL DEFAULT 'TEXT',"
+                            + "PRIMARY KEY(_id))");
+            database.execSQL("INSERT INTO categories_new(_id, name) SELECT _id, name FROM categories");
+            database.execSQL("DROP TABLE categories");
+            database.execSQL("ALTER TABLE categories_new RENAME TO categories");
+
         }
     };
 
