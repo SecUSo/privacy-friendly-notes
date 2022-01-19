dfbb20eeccb73768bc4bf339742db833ac2f5360
maxmitz <m-zimmermann1@gmx.net>
2022-01-19 16:10:50 +0100

    Added license

--- app/src/main/java/org/secuso/privacyfriendlynotes/room/adapter/NoteAdapter.java
+++ app/src/main/java/org/secuso/privacyfriendlynotes/room/adapter/NoteAdapter.java
@@ -1,3 +1,16 @@
+/*
+ This file is part of the application Privacy Friendly Notes.
+ Privacy Friendly Notes is free software:
+ you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation,
+ either version 3 of the License, or any later version.
+ Privacy Friendly Notes is distributed in the hope
+ that it will be useful, but WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with Privacy Friendly Notes. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.secuso.privacyfriendlynotes.room.adapter;
 
 import android.graphics.drawable.BitmapDrawable;
--- app/src/main/java/org/secuso/privacyfriendlynotes/ui/TutorialActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlynotes/ui/TutorialActivity.java
@@ -1,18 +1,15 @@
 /*
- This file is part of Privacy Friendly App Example.
-
- Privacy Friendly App Example is free software:
+ This file is part of the application Privacy Friendly Notes.
+ Privacy Friendly Notes is free software:
  you can redistribute it and/or modify it under the terms of the
  GNU General Public License as published by the Free Software Foundation,
  either version 3 of the License, or any later version.
-
- Privacy Friendly App Example is distributed in the hope
+ Privacy Friendly Notes is distributed in the hope
  that it will be useful, but WITHOUT ANY WARRANTY; without even
  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  See the GNU General Public License for more details.
-
  You should have received a copy of the GNU General Public License
- along with Privacy Friendly App Example. If not, see <http://www.gnu.org/licenses/>.
+ along with Privacy Friendly Notes. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.secuso.privacyfriendlynotes.ui;
 
