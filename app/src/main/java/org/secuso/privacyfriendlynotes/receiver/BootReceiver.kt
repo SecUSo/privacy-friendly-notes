6c327140a8fd56a68dd6a446a123289ae7114da1
Tobias Länge <tobias.laenge9@kit.edu>
2022-08-07 20:06:16 +0200

    Fix database access from main thread crash in BootReceiver
    - Remove code used for debugging
    - This still blocks the main thread but prevents the crash

--- app/src/main/java/org/secuso/privacyfriendlynotes/receiver/BootReceiver.kt
+++ app/src/main/java/org/secuso/privacyfriendlynotes/receiver/BootReceiver.kt
@@ -18,8 +18,8 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.secuso.privacyfriendlynotes.room.NoteDatabase
 import org.secuso.privacyfriendlynotes.room.model.Notification
 import org.secuso.privacyfriendlynotes.ui.helper.NotificationHelper
@@ -27,20 +27,21 @@ import org.secuso.privacyfriendlynotes.ui.helper.NotificationHelper
 class BootReceiver : BroadcastReceiver() {
     var allNotifications: List<Notification>? = null
     override fun onReceive(context: Context, intent: Intent) {
-        if (Intent.ACTION_BOOT_COMPLETED != intent.action && Intent.ACTION_AIRPLANE_MODE_CHANGED != intent.action) return
+        if (Intent.ACTION_BOOT_COMPLETED != intent.action) return
 
         Log.d(javaClass.simpleName, "Running onReceive...")
-        GlobalScope.launch(Dispatchers.Main) {
+        runBlocking {
+            launch(Dispatchers.IO) {
+                allNotifications = NoteDatabase.getInstance(context).notificationDao().getAllNotifications()
 
-            allNotifications = NoteDatabase.getInstance(context).notificationDao().getAllNotifications()
+                Log.d(javaClass.simpleName, allNotifications!!.size.toString() + " Notifications found.")
+                for ((notification_id, time) in allNotifications!!) {
 
-            Log.d(javaClass.simpleName, allNotifications!!.size.toString() + " Notifications found.")
-            for ((notification_id, time) in allNotifications!!) {
-
-                val alarmTime = time.toLong()
-                val note = NoteDatabase.getInstance(context).noteDao().getNoteByID(notification_id.toLong())
-                note?.let {
-                    NotificationHelper.addNotificationToAlarmManager(context, note._id, note.type, note.name, alarmTime)
+                    val alarmTime = time.toLong()
+                    val note = NoteDatabase.getInstance(context).noteDao().getNoteByID(notification_id.toLong())
+                    note?.let {
+                        NotificationHelper.addNotificationToAlarmManager(context, note._id, note.type, note.name, alarmTime)
+                    }
                 }
             }
         }
